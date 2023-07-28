6b2eec5c8fb3eeb474026db77c9760a373b2366f
Becky Smith <rebkwok@gmail.com>
2023-07-28 23:21:23 +0100

    Collections menu item for moderators

--- dashboard/wagtail_hooks.py
+++ dashboard/wagtail_hooks.py
@@ -1,4 +1,8 @@
+from django.urls import reverse
+
 from wagtail import hooks
+from wagtail.admin.menu import MenuItem
+
 from wagtail.admin.userbar import AccessibilityItem
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin,
@@ -20,6 +24,11 @@ class FooterSnippetViewSetGroup(SnippetViewSetGroup):
     menu_label = "Footer Text"
     menu_order = 200  # will put in 4th place (000 being 1st, 100 2nd)
     items = (FooterTextViewSet, )
+
+
+@hooks.register('register_admin_menu_item')
+def register_collections_menu_item():
+  return MenuItem('Image Collections', reverse('wagtailadmin_collections:index'), icon_name='folder-inverse', order=10000)
 
 
 register_snippet(FooterSnippetViewSetGroup)
