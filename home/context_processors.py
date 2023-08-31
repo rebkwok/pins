96498f8964c0a92e0faac45ef130c5160ffeb218
Becky Smith <rebkwok@gmail.com>
2023-08-31 16:09:45 +0100

    option to hide breadcrumbs

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,5 +3,6 @@ import os
 
 def home(request):
     return {
-        "hide_search": os.environ.get("HIDE_SEARCH", False)
+        "hide_search": os.environ.get("HIDE_SEARCH", False),
+        "hide_breadcrumbs": os.environ.get("HIDE_BREADCRUMBS", False),
     }
