690df358b6c27d06d0669fb2279d06ef3955e2fb
Becky Smith <rebkwok@gmail.com>
2023-10-10 16:23:03 +0100

    Email report option

--- dogs/management/commands/facebook_report.py
+++ dogs/management/commands/facebook_report.py
@@ -1,4 +1,7 @@
-from django.core.management.base import BaseCommand
+from datetime import datetime
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandParser
+from django.core.mail import send_mail
 
 from dogs.models import FacebookAlbumTracker
 
@@ -6,19 +9,40 @@ from dogs.models import FacebookAlbumTracker
 class Command(BaseCommand):
     help = 'report Facebook changes'
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--email",
+            action="store_true",
+            help="Send to support email",
+            default=False,
+        )
+        return super().add_arguments(parser)
+
+    def handle(self, email, **kwargs):
         tracker = FacebookAlbumTracker()
         new_data = tracker.fetch_all()
         changes = tracker.report_changes(new_data)
 
+        mail_content = [f"Facebook album changes as of {datetime.utcnow()}"]
         for change, changed_data in changes.items():
             if changed_data:
-                print("==============================================")
-                print(change)
-                print("==============================================")
+                mail_content.append("\n==============================================")
+                mail_content.append(change)
+                mail_content.append("==============================================")
             for key, val in changed_data.items():
                 if change != "custom_albums":
-                    print(f"{key}: {val}")
+                    mail_content.append(f"{key}: {val}")
                 else:
-                    print(key)
-            print("\n")
+                    mail_content.append(f"{key}: {val.get('link', '')}")
+        
+        mail_content = "\n".join(mail_content)
+        self.stdout.write(mail_content)
+        if email:
+            send_mail(
+                subject="Facebook album report",
+                message=mail_content,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[settings.SUPPORT_EMAIL]
+            )
+
+
