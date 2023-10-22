7e2e0efba91d4dd0adb2ca01daaa84a0879f926e
Becky Smith <rebkwok@gmail.com>
2023-10-22 17:53:30 +0100

    Add some output logging

--- dogs/management/commands/remove_expired_images.py
+++ dogs/management/commands/remove_expired_images.py
@@ -20,21 +20,26 @@ class Command(BaseCommand):
                 "link": album["link"],
                 "count": album["count"],
                 "name": album["name"],
-                "description": album.get("description"),
-                "updated_time": album.get("updated_time"),
                 "images": []
             }
+            for field in ["description", "updated_time"]: 
+                if field in album:
+                    new_albums_data[album_id][field] = album[field]
             self.stdout.write(f"Checking album {i + 1} of {total}")
 
             images = []
+            self.stdout.write(f"Existing image count: {len(album['images'])}")
             for image in tqdm(album["images"], desc=f"Checking images for {album_id}"):
                 try:
                     resp = requests.get(image["image_url"])
                     if resp.status_code == 200:
                         images.append(image)
+                    else:
+                        self.stdout.write("Expired url")
                 except requests.exceptions.ConnectionError:
                     # ignore connection error, happens when the URL is no longer 
                     # accessible at all
-                    ...
+                    self.stdout.write("Inaccessible url")
+            self.stdout.write(f"New image count: {len(images)}")
             new_albums_data[album_id]["images"] = images
         albums_container.update_all(new_albums_data)
