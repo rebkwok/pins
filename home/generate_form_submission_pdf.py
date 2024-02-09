ee53d67a84b8ec15423c59318fc8f87c3f0bbc3d
Becky Smith <rebkwok@gmail.com>
2024-02-09 10:05:15 +0000

    indent in PDFs

--- home/generate_form_submission_pdf.py
+++ home/generate_form_submission_pdf.py
@@ -34,8 +34,31 @@ def generate_pdf(submission):
             name='Response', fontSize=10, textColor="#00008B"
     ))
 
+    def normal_paragraph_style_with_indent(spaces):
+        if spaces == 0:
+            return styles["Normal"]
+        style_name = f"n_with_{spaces}_indent"
+        if style_name not in styles:
+            styles.add(
+                ParagraphStyle(
+                    style_name, fontSize=10, leftIndent=spaces*5
+                )
+            )
+        return styles[style_name]
 
-
+    def add_info_text(story_lines, field, info_location):
+        info_text = html2text(submission.form_page.form_field_info_texts[field][info_location])
+        for line in info_text.split('\n'):
+            leading_space = len(line) - len(line.lstrip())
+            story_lines.append(
+                Paragraph(
+                    line, 
+                    normal_paragraph_style_with_indent(leading_space)
+                )
+            )
+        story_lines.append(Spacer(1, 5))
+        return story_lines
+    
     story = []
     title = submission.page.title
     submitted_label = "Submitted"
@@ -66,22 +89,15 @@ def generate_pdf(submission):
         )
 
         if field in submission.form_page.form_field_info_texts:
-            before = html2text(submission.form_page.form_field_info_texts[field]["before"])
-            for line in before.split('\n'):
-                story.append(Paragraph(line, styles["Normal"]))
-            story.append(Spacer(1, 5))
+            story = add_info_text(story, field, "before")
 
         for line in value.split('\n'):
             story.append(Paragraph(line, styles["Response"]))
 
         if field in submission.form_page.form_field_info_texts:
-            story.append(Spacer(1, 5))
-            after = html2text(submission.form_page.form_field_info_texts[field]["after"])
-            for line in after.split('\n'):
-                story.append(Paragraph(line, styles["Normal"]))
+            story = add_info_text(story, field, "after")
 
         story.append(Spacer(1, 10))
-
 
     doc.build(story)
 
