463e3c69e58a278b54b3e31b8b29ba5ac52a8f31
Becky Smith <rebkwok@gmail.com>
2024-02-26 08:54:06 +0000

    close recipe submissions

--- fundraising/context_processors.py
+++ fundraising/context_processors.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from .models import RecipeBookSubmission, PAGE_TYPE_PAGE_COUNTS
 
 
@@ -5,5 +6,6 @@ def recipes(request):
     return {
         "total_recipe_book_pages": sum([PAGE_TYPE_PAGE_COUNTS[subm.page_type] for subm in RecipeBookSubmission.objects.all()]),
         "total_recipe_book_money": sum([subm.cost for subm in RecipeBookSubmission.objects.all()]),
-        "total_recipe_book_pending": sum([subm.cost for subm in RecipeBookSubmission.objects.filter(paid=False)]) 
+        "total_recipe_book_pending": sum([subm.cost for subm in RecipeBookSubmission.objects.filter(paid=False)]) ,
+        "recipe_submissions_open": settings.RECIPE_SUBMISSIONS_OPEN,
     }
