3e2f1913f953317d672f2e7c93bf2cbfc3301f00
Becky Smith <rebkwok@gmail.com>
2024-07-11 10:43:09 +0100

    add a facebook check manage command

---
+++ dogs/management/commands/facebook_check.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from dogs.models import FacebookAlbumTracker, DogPage, DogStatusPage
+
+
+class Command(BaseCommand):
+    help = 'Check stored facebook data'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--email",
+            action="store_true",
+            help="Send to support email",
+        )
+        return super().add_arguments(parser)
+
+    def handle(self, email, **kwargs):
+        tracker = FacebookAlbumTracker()
+
+        for status_page in DogStatusPage.objects.all():
+            self.stdout.write(f"============{status_page.title}===========")
+            dog_pages = status_page.get_children()
+            for page in dog_pages:
+                saved_data = tracker.albums_obj.get_album(page.specific.facebook_album_id)
+                if not saved_data:
+                    self.stdout.write(f"Site: {page} - {page.get_parent().title} / Facenook: removed")
+                else:
+                    self.stdout.write(f"Site: {page.title} - {page.get_parent().title} / Facebook: - {saved_data['name']}")
