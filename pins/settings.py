8584ddcdb91be72973474dbc33874d8b0262d44f
Becky Smith <rebkwok@gmail.com>
2026-01-10 14:22:14 +0000

    Fix ADMINS setting for django 6.0

--- pins/settings.py
+++ pins/settings.py
@@ -453,7 +453,7 @@ else:
         },
     }
 
-ADMINS = [("Becky Smith", SUPPORT_EMAIL)]
+ADMINS = [SUPPORT_EMAIL]
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
